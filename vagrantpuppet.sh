cff6f3d6f0a1753e02cd35215f8690bc6f48e9e2
ronde21 <a1700601@myy.haaga-helia.fi>
2017-11-12 16:52:35 +0000

    Update vagrantpuppet.sh

--- vagrantpuppet.sh
+++ vagrantpuppet.sh
@@ -1,7 +1,7 @@
 sudo apt-get update
 sudo apt-get -y install puppet
-echo "192.168.43.147 master" |sudo tee --append /etc/hosts
+echo "192.168.1.40 master.local" |sudo tee --append /etc/hosts
 echo "[agent]"|sudo tee --append /etc/puppet/puppet.conf
-echo "server = master"|sudo tee --append /etc/puppet/puppet.conf
+echo "server = master.local"|sudo tee --append /etc/puppet/puppet.conf
 sudo puppet agent --enable
 sudo service puppet restart
